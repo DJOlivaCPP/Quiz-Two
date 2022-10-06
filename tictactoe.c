eddcf36a5a30cc3d40435dd11461a5e7dbafb082
Dylan Oliva <djoliva@cpp.edu>
2022-10-05 19:29:44 -0700

    added a few variables to prepare the board state

--- tictactoe.c
+++ tictactoe.c
@@ -16,11 +16,25 @@ int main(){
     //prompt user:
     // vs CPU or vs Player2
 
-    input val;
+    input in;
+    int win = 0;
     printf("Enter a value to play against CPU or Player");
     printf("1 - CPU");
     printf("2 - Player 2");
-    val = getchar();
+    in = getchar();
+
+    //preparing board state
+    char diveder[] = "+-----+";
+    char val1 = '1';
+    char val2 = '2';
+    char val3 = '3';
+    char val4 = '4';
+    char val5 = '5';
+    char val6 = '6';
+    char val7 = '7';
+    char val8 = '8';
+    char val9 = '9';
+
 
     //read information from console
 
