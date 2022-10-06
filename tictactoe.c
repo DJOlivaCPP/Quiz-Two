9a37dda959e9f91bcfec4b551925b2320c6df43e
Dylan Oliva <djoliva@cpp.edu>
2022-10-05 19:16:57 -0700

    Created initla file with charecter input

---
+++ tictactoe.c
@@ -0,0 +1,31 @@
+/*
++-----+
+| | | |
++-----+
+| | | |
++-----+
+| | | |
++-----+
+*/
+
+#include <stdio.h>
+
+typedef int input;
+
+int main(){
+    //prompt user:
+    // vs CPU or vs Player2
+
+    input val;
+    printf("Enter a value to play against CPU or Player");
+    printf("1 - CPU");
+    printf("2 - Player 2");
+    val = getchar();
+
+    //read information from console
+
+    //compute win states
+    //display winner if win state
+
+    return 0;
+}
