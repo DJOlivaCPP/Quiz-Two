d2d5e641a0ddf8cf7d808c8ce53c22dafd57d3d4
Dylan Oliva <djoliva@cpp.edu>
2022-10-07 12:45:47 -0700

    added player move methods and code to check if player one has won

--- tictactoe.c
+++ tictactoe.c
@@ -9,15 +9,31 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 typedef int input;
 
-int main(){
+int end = 1;
+char val1 = '1';
+char val2 = '2';
+char val3 = '3';
+char val4 = '4';
+char val5 = '5';
+char val6 = '6';
+char val7 = '7';
+char val8 = '8';
+char val9 = '9';
+
+int main()
+{
     // prompt user:
     //  vs CPU or vs Player2
 
     input in;
-    int end = 1;
+    char cpu;
+    time_t t;
+    srand((unsigned)time(&t));
     printf("Enter a value to play against CPU or Player");
     printf("1 - CPU");
     printf("2 - Player 2");
@@ -25,20 +41,13 @@ int main(){
 
     // preparing board state
     char divider[] = "+-----+";
-    char val1 = '1';
-    char val2 = '2';
-    char val3 = '3';
-    char val4 = '4';
-    char val5 = '5';
-    char val6 = '6';
-    char val7 = '7';
-    char val8 = '8';
-    char val9 = '9';
 
     // vs CPU
     // while loop to keep game runnning until tie or win
-    if (in == 1){
-        while (end == 1){
+    if (in == 1)
+    {
+        while (end == 1)
+        {
             printf("Board State:");
             printf(divider);
             printf("|%s|%s|%s|", val1, val2, val3);
@@ -47,66 +56,174 @@ int main(){
             printf("Where would you like to put an X:");
             in = getchar();
 
-            switch(in){
-                case '1':
-                    val1 = 'X';
-                    break;
-                case '2':
-                    val2 = 'X';
-                    break;
-                case '3':
-                    val3 = 'X';
-                    break;
-                case '4':
-                    val4 = 'X';
-                    break;
-                case '5':
-                    val5 = 'X';
-                    break;
-                case '6':
-                    val6 = 'X';
-                    break;
-                case '7':
-                    val7 = 'X';
-                    break;
-                case '8':
-                    val8 = 'X';
-                    break;
-                case '9':
-                    val9 = 'X';
-                    break;
+            playerMove(in);
+
+            // check if player wins
+            if (val1 == 'X' && val2 == 'X' && val3 == 'X')
+            {
+                end = 0;
+                printf("Player Wins!");
+            }
+            else if (val4 == 'X' && val5 == 'X' && val6 == 'X')
+            {
+                end = 0;
+                printf("Player Wins!");
+            }
+            else if (val7 == 'X' && val8 == 'X' && val9 == 'X')
+            {
+                end = 0;
+                printf("Player Wins!");
+            }
+            else if (val1 == 'X' && val4 == 'X' && val7 == 'X')
+            {
+                end = 0;
+                printf("Player Wins!");
+            }
+            else if (val2 == 'X' && val5 == 'X' && val8 == 'X')
+            {
+                end = 0;
+                printf("Player Wins!");
+            }
+            else if (val3 == 'X' && val6 == 'X' && val9 == 'X')
+            {
+                end = 0;
+                printf("Player Wins!");
+            }
+            else if (val4 == 'X' && val5 == 'X' && val6 == 'X')
+            {
+                end = 0;
+                printf("Player Wins!");
+            }
+            else if (val1 == 'X' && val5 == 'X' && val9 == 'X')
+            {
+                end = 0;
+                printf("Player Wins!");
+            }
+            else if (val3 == 'X' && val5 == 'X' && val7 == 'X')
+            {
+                end = 0;
+                printf("Player Wins!");
             }
 
-            //check if player wins
-            if(val1 == 'X' && val2 == 'X' && val3 == 'X') {
-                end = 0;
-                printf("You win!");
-            } else if (val4 == 'X' && val5 == 'X' && val6 == 'X') {
-                end = 0;
-                printf("You win!");
-            } else if (val7 == 'X' && val8 == 'X' && val9 == 'X') {
-                end = 0;
-                printf("You win!");
-            } else if (val1 == 'X' && val4 == 'X' && val7 == 'X') {
-                end = 0;
-                printf("You win!");
-            } else if (val2 == 'X' && val5 == 'X' && val8 == 'X') {
-                end = 0;
-                printf("You win!");
-            } else if (val3 == 'X' && val6 == 'X' && val9 == 'X') {
-                end = 0;
-                printf("You win!");
-            } else if (val4 == 'X' && val5 == 'X' && val6 == 'X') {
-                end = 0;
-                printf("You win!");
-            } else if (val1 == 'X' && val5 == 'X' && val9 == 'X') {
-                end = 0;
-                printf("You win!");
-            } else if (val3 == 'X' && val5 == 'X' && val7 == 'X') {
-                end = 0;
-                printf("You win!");
-            }
+            moveCPU(in);
         }
     }
+
     return 0;
+}
+
+int playerMove(int in)
+{
+    switch (in)
+    {
+    case '1':
+        val1 = 'X';
+        break;
+    case '2':
+        val2 = 'X';
+        break;
+    case '3':
+        val3 = 'X';
+        break;
+    case '4':
+        val4 = 'X';
+        break;
+    case '5':
+        val5 = 'X';
+        break;
+    case '6':
+        val6 = 'X';
+        break;
+    case '7':
+        val7 = 'X';
+        break;
+    case '8':
+        val8 = 'X';
+        break;
+    case '9':
+        val9 = 'X';
+        break;
+    }
+
+    return 1;
+}
+
+int player2Move(int in)
+{
+    switch (in)
+    {
+    case '1':
+        val1 = 'O';
+        break;
+    case '2':
+        val2 = 'O';
+        break;
+    case '3':
+        val3 = 'O';
+        break;
+    case '4':
+        val4 = 'O';
+        break;
+    case '5':
+        val5 = 'O';
+        break;
+    case '6':
+        val6 = 'O';
+        break;
+    case '7':
+        val7 = 'O';
+        break;
+    case '8':
+        val8 = 'O';
+        break;
+    case '9':
+        val9 = 'O';
+        break;
+    }
+
+    return 1;
+}
+
+int moveCPU()
+{
+    int valid = 0;
+    int move;
+
+    do
+    {
+        move = rand() % 9;
+
+        if (move == 1 && val1 != 'X')
+        {
+            valid = 1;
+            player2Move(move);
+        } else if (move == 2 && val2 != 'X'){
+            valid = 1;
+            player2Move(move);
+        } else if (move == 3 && val3 != 'X'){
+            valid = 1;
+            player2Move(move);
+        } else if (move == 4 && val4 != 'X'){
+            valid = 1;
+            player2Move(move);
+        } else if (move == 5 && val5 != 'X'){
+            valid = 1;
+            player2Move(move);
+        }else if (move == 6 && val6 != 'X'){
+            valid = 1;
+            player2Move(move);
+        }else if (move == 7 && val7 != 'X'){
+            valid = 1;
+            player2Move(move);
+        }else if (move == 8 && val8 != 'X'){
+            valid = 1;
+            player2Move(move);
+        }else if (move == 9 && val9 != 'X'){
+            valid = 1;
+            player2Move(move);
+        }
+        
+    } while (valid == 0);
+
+    return move;
 }
